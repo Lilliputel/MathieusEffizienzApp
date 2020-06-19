0b2c5d0a398246b52637054e50b4bbcf21b8bda1
Mathieu Lichtsteiner <m.lichtsteiner@outlook.de>
2020-06-19 22:53:24 +0200

    Treeview in Projektübersicht moved into a TabControl, also added a Dictionary for TreeView

--- EffizienzDesktop/ViewModels/Main/ViewModelProjektÜbersicht.cs
+++ EffizienzDesktop/ViewModels/Main/ViewModelProjektÜbersicht.cs
@@ -1,16 +1,14 @@
 ﻿using Effizienz.Classes;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Windows;
 
 namespace Effizienz.Views {
 	public class ViewModelProjektÜbersicht : ViewModelBase {
 
 		#region properties
-		public ObservableCollection<Projekt> Projekte => new ObservableCollection<Projekt>(from Kategorie in ( Application.Current as App ).KategorienListe
-																						   select Kategorie.Projekte into ProjektListe
-																						   from Projekt in ProjektListe
-																						   select Projekt);
+
+		public ObservableCollection<Kategorie> Kategorien
+			=> ( Application.Current as App ).KategorienListe;
 
 		#endregion
 
