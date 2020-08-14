b34f5651d419d0c923cab7a78ebc31be3ee6a7a5
Mathieu Lichtsteiner <m.lichtsteiner@outlook.de>
2020-08-14 14:57:16 +0200

    Changed DayPlanView to HeaderedItemsControl instead of UserControl

--- FrontLayer/Components/DayPlanView.xaml.cs
+++ FrontLayer/Components/DayPlanView.xaml.cs
@@ -1,38 +1,11 @@
-﻿using ModelLayer.Planning;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace FrontLayer.Components {
 
-	public partial class DayPlanView : UserControl {
+	public partial class DayPlanView : HeaderedItemsControl {
 
 		public DayPlanView() {
 			InitializeComponent();
 		}
-
-		public DayPlan DayPlan {
-			get { return (DayPlan)GetValue(DayPlanProperty); }
-			set { SetValue(DayPlanProperty, value); }
-		}
-
-		public static readonly DependencyProperty DayPlanProperty =
-			DependencyProperty.Register("DayPlan", typeof(DayPlan), typeof(DayPlanView));
-
-		public DataTemplate HeaderTemplate {
-			get { return (DataTemplate)GetValue(HeaderTemplateProperty); }
-			set { SetValue(HeaderTemplateProperty, value); }
-		}
-
-		public static readonly DependencyProperty HeaderTemplateProperty =
-			DependencyProperty.Register("HeaderTemplate", typeof(DataTemplate), typeof(DayPlanView));
-
-		public object Header {
-			get { return (object)GetValue(HeaderProperty); }
-			set { SetValue(HeaderProperty, value); }
-		}
-
-		public static readonly DependencyProperty HeaderProperty =
-			DependencyProperty.Register("Header", typeof(object), typeof(DayPlanView));
-
 	}
 }
