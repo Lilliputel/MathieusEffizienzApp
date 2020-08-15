133e1316ac8bf02ffe7e999d8683f91a7adfc114
Mathieu Lichtsteiner <m.lichtsteiner@outlook.de>
2020-08-15 13:36:50 +0200

    Experimented with the auto generated mouseDown event

--- FrontLayer/Components/DayPlanView.xaml.cs
+++ FrontLayer/Components/DayPlanView.xaml.cs
@@ -7,5 +7,9 @@ namespace FrontLayer.Components {
 		public DayPlanView() {
 			InitializeComponent();
 		}
+
+		private void Root_MouseDown( object sender, System.Windows.Input.MouseButtonEventArgs e ) {
+			double y = e.GetPosition(this).Y;
+		}
 	}
 }
