4da4e767557a14bae31e9fcfe1e9adf441ed5fec
Mathieu Lichtsteiner <Mathieu-Lichtsteiner@users.noreply.github.com>
2021-01-26 16:54:50 +0100

    Implemented a Combobox with a TreeView in the Dropdown

---
+++ EffizienzControls/Converters/InvertBool.cs
@@ -0,0 +1,10 @@
+﻿using EffizienzControls.Extensions;
+using System;
+using System.Globalization;
+
+namespace EffizienzControls.Converters {
+	public class InvertBool : MarkedupValueConverter<InvertBool> {
+		public override object? Convert( object? value, Type targetType, object parameter, CultureInfo culture )
+			=> !(value as bool?);
+	}
+}
