e1af54b0bb26f291d480f425f16cbda0ac0bbaba
Mathieu Lichtsteiner <Mathieu-Lichtsteiner@users.noreply.github.com>
2021-01-26 18:44:38 +0100

    Copied the Default-ComboBox Template to a new Custom Control "TreeViewBox", where I will implement my Control cleanly

---
+++ EffizienzControls/TreeViewBox/TreeViewBox.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EffizienzControls {
+	public class TreeViewBox : ComboBox {
+		static TreeViewBox()
+			=> DefaultStyleKeyProperty.OverrideMetadata( typeof( TreeViewBox ), new FrameworkPropertyMetadata( typeof( TreeViewBox ) ) );
+
+
+	}
+}
