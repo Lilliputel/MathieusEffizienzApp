526522ccb69df058e3a141edcf57a0273f8b6539
Mathieu Lichtsteiner <Mathieu-Lichtsteiner@users.noreply.github.com>
2021-02-06 14:57:31 +0100

    - Fixed Naming-Violation in ListExtensions - Fixed Binding-Errors in NewCategoryView by implementiing a ClearTemplateselector, what returns null, if the SelectedValue is null - Fixed Bug in the Binding "SelectedItem" in NewGoalView

---
+++ FrontLayer.WPF/TemplateSelectors/ClearTemplateSelector.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FrontLayer.WPF.TemplateSelectors {
+	public class ClearTemplateSelector : DataTemplateSelector {
+		public DataTemplate? ClearTemplate => null;
+		public DataTemplate? ItemTemplate { get; set; }
+		public override DataTemplate? SelectTemplate( object? item, DependencyObject container )
+			=> (item is null) ? ClearTemplate : ItemTemplate;
+	}
+}
--- ModelLayer/Extensions/ListExtensions.cs
+++ ModelLayer/Extensions/ListExtensions.cs
@@ -4,22 +4,22 @@ namespace ModelLayer.Extensions {
 
 	public static class ListExtensions {
 
-		public static ICollection<T>? AddUnique<T>( this ICollection<T>? extension, T Item ) {
-			if( extension is ICollection<T> && extension.Contains( Item ) is false )
-				extension.Add( Item );
+		public static ICollection<T>? AddUnique<T>( this ICollection<T>? extension, T item ) {
+			if( extension is ICollection<T> && extension.Contains( item ) is false )
+				extension.Add( item );
 			return extension;
 		}
 
-		public static ICollection<T>? AddUniqueRange<T>( this ICollection<T>? extension, ICollection<T>? Collection ) {
-			if( extension is ICollection<T> && Collection is ICollection<T> )
-				foreach( T item in Collection )
+		public static ICollection<T>? AddUniqueRange<T>( this ICollection<T>? extension, ICollection<T>? collection ) {
+			if( extension is ICollection<T> && collection is ICollection<T> )
+				foreach( T item in collection )
 					extension.AddUnique( item );
 			return extension;
 		}
 
-		public static ICollection<T>? AddRange<T>( this ICollection<T>? extension, ICollection<T>? Collection ) {
-			if( extension is ICollection<T> && Collection is ICollection<T> )
-				foreach( T item in Collection )
+		public static ICollection<T>? AddRange<T>( this ICollection<T>? extension, ICollection<T>? collection ) {
+			if( extension is ICollection<T> && collection is ICollection<T> )
+				foreach( T item in collection )
 					extension.Add( item );
 			return extension;
 		}
